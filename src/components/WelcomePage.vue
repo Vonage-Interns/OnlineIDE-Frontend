3b508eed880a69015d00f3110369d5c3acb6b33f
pravalli-pamu <pamu.pravallika@vonage.com>
2025-09-03 15:37:36 +0530

    online IDE Frontend files

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="welcome">
+    <div :style="content">
+    <h1>{{en.hi}}{{username}}🎉</h1>
+    <h1>{{en.welcome_to_vonage}}</h1>
+    </div>
+    </div>
+</template>
+
+<script>
+import en from '@/locales/en';
+import StorageWrapper from '../store/storageWrapper';
+
+export default{
+  data(){
+    return{
+      en,
+    }
+  },
+
+ computed:{
+  username(){
+    const email=StorageWrapper.get('email')
+    return email.split('@')[0];
+  },
+
+  content(){
+    const isopen=this.$store.state.FileStore.slideropen
+    return {
+     marginLeft:isopen?'-150px':'90px',
+     paddingTop:'300px'
+    }
+  }
+ }
+}
+</script>
+
+<style scoped>
+.welcome{
+  height:100vh; 
+  width: 1440px;
+  font-size:20px;
+  background:linear-gradient(skyblue,pink);
+  
+}
+
+.welcome h1{
+    color:black;
+    text-align:center
+}
+</style>
